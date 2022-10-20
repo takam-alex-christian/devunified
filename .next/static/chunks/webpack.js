fbad51e0f447c6c78d81b74518786ddd598091cc
takam-alex-christian <100483232+takam-alex-christian@users.noreply.github.com>
2022-10-20 19:40:27 +0100

    excluding .next folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e8a04cbd0ba0e7bc"; }
+/******/ 		__webpack_require__.h = function() { return "57347ea89ca150c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
